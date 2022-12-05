7ade7f04e342389a35240fb54bfe472cef4f05ce
cadanu <gdavidjoyce@gmail.com>
2022-12-05 09:19:33 -0500

    Completed useState

--- basics/src/components/counter/Counter.js
+++ basics/src/components/counter/Counter.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Number from './Number';
 import CounterButton from './CounterButton';
 
@@ -6,15 +6,44 @@ import CounterButton from './CounterButton';
 const Counter = (props) => {
 
     const [points, setPoints] = useState(0);
+    const [goal, setGoal] = useState(11);
     // console.log(useState(0));
+    useEffect(() => {
+        console.log('Runs every render or on data change.');
+    })
+    useEffect(() => {
+        console.log('Run on first render only');
+    }, []);
+    useEffect(() => {
+        console.log('Runs when goal is hit');
+    }, [goal])
 
     return (
-        <div className='counter'>
+        <div className='counter' style={{position: 'relative'}}>
             <Number digit={points}/>
             <div className='counter__buttons'>
-                <CounterButton operator="minus" points={points} checkPoints={setPoints} className='minus'>-</CounterButton>
-                <CounterButton operator="plus" points={points} checkPoints={setPoints} className='plus'>+</CounterButton>
+                <CounterButton
+                    operator="minus"
+                    checkPoints={setPoints}
+                    number={points}
+                    className='minus'
+                    setGoal={setGoal}
+                    myGoal={goal}
+                >
+                    -
+                </CounterButton>
+                <CounterButton
+                    operator="plus"
+                    checkPoints={setPoints}
+                    number={points}
+                    className='plus'
+                    setGoal={setGoal}
+                    myGoal={goal}
+                >
+                    +
+                </CounterButton>
             </div>
+            <span style={{fontSize: 'larger', padding: '5px', color: 'yellow'}}>Next goal is {goal}</span>
         </div>
     )
 
--- basics/src/components/counter/CounterButton.js
+++ basics/src/components/counter/CounterButton.js
@@ -6,20 +6,46 @@ const CounterButton = (props) => {
     // const clickButton = () => {
     //     console.log('got clicked');
     // }
+    // props.setPoints = (foo) => {
+    //     alert('we in this');
+    // }
+    let localGoal;
+
 
     const checkButton = () => {
-        if (props.operator == 'minus') {
-            // console.log(props.operator);
-            props.setPoints(0);
+        localGoal = props.goal;
+
+        if ((props.operator === 'minus') && (props.number > 0)) {
+            console.log(props.checkPoints); 
+            props.checkPoints(props.number - 1);
         }
-        if (props.operator == 'plus') {
-            // console.log(props.operator);
-            props.setPoints(1);
+
+        if (props.operator === 'plus') {
+            console.log(props.checkPoints);
+            props.checkPoints(props.number + 1);
+
+            if (props.myGoal == props.number+1) {
+                // alert('hit goal' + props.number);
+                localGoal = props.number;
+                props.setGoal((props.number+11)*2)
+            }
         }
+
+
+        // if (props.myGoal == props.number) {
+        //     console.log('inside goal');
+        //     if ((props.operator === 'minus') && (props.number > 0)) {
+        //         alert('hit goal going down');
+        //     }
+        //     if (props.operator === 'plus') {
+        //         alert('hit goal going up');
+        //     }
+        // }
     }
 
     return (
-        <button className='counter__button' onClick={checkButton}>{props.children}</button>
+        <button className='counter__button'  onMouseUp={checkButton} onKeyDown={checkButton}>{props.children}</button>
+
     )
 }
 
